7ec58b720b9d17c0e375793825d14687beeff0a2
PC10 <PC10@INFO5-5381>
2022-03-16 10:30:34 +0100

    modifica delle property e dei relativi punti

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -10,23 +10,41 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     {
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
-        public int Codice { public get; private set; }
-        public string Descrizione{ public get; private set; }
-        public string UnitaMisura { public get; private set; }
-        public double Prezzo { public get; private set; }
-
+        private int codice;
+        private string descrizione;
+        private string unitaMisura;
+        private double prezzo;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
+
+        public int Codice
+        {
+            get { return codice; }
+        }
+        public string Descrizione
+        {
+            get { return descrizione; }
+        }
+
+        public string UnitaMisura
+        {
+            get { return unitaMisura; }
+        }
+
+        public double Prezzo
+        {
+            get { return codice; }
+        }
 
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
-            Codice = codice;
-            Descrizione = descrizione;
-            UnitaMisura = unitaMisura;
-            Prezzo = prezzo;
+            this.codice = codice;
+            this.descrizione = descrizione;
+            this.unitaMisura = unitaMisura;
+            this.prezzo = prezzo;
         }
 
         public string Visualizzati()
--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -16,15 +16,31 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
         // ...
-        private string Descrizione { get; private set; }
-        private string UnitaMisura { get; private set; }
-        private double Prezzo { get; private set; } 
+        private string descrizione;
+        private string unitaMisura;
+        private double prezzo;
         
 
         public DialogResult Status { get { return status; } }
 
         // TODO: (6) aggiungere property di sola lettura dei dati inseriti nella frmArticoli per l'utilizzo in frmMain
         // ...
+        
+        public string Descrizione
+        {
+            get { return descrizione; }
+        }
+
+        public string UnitaMisura
+        {
+            get { return unitaMisura; }
+        }
+
+        public double Prezzo
+        {
+            get { return prezzo; }
+        }
+           
         public frmArticolo()
         {
             InitializeComponent();
@@ -36,9 +52,9 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
             // ... descrizione, unitaMisura, prezzo
             if (txtPrezzo.text != "" && txtDescrizione.text != "" & cmbUnitaMisura.text != "")
             {
-                Descrizione = txtDescrizione.text;
-                UnitaMisura = cmbUnitaMisura;
-                Prezzo = txtPrezzo;
+                descrizione = txtDescrizione.text;
+                unitaMisura = cmbUnitaMisura;
+                prezzo = txtPrezzo;
             }
             else
             {
